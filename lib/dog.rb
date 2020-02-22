7f9e512cd3ebed2d07a597d9b98ff4f0b4942a5f
Michaylah Mam <mchylh@gmail.com>
2020-02-22 18:16:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
 class Dog
   def bark
     puts "Woof!"
+    fido = Dog.new
+    fido.bark
   end
 end
-fido = Dog.new
-fido.bark
 
   def sit
   "The Dog is sitting."
+  fido = Dog.new
+  fido.sit
   end
 end
