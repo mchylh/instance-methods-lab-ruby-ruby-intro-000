f6d2cef41302f72fb92ffe10b03ba07994b8dac9
Michaylah Mam <mchylh@gmail.com>
2020-02-22 18:24:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
   def talk
-    "Hello World!"
+    puts "Hello World!"
     ada = Person.new
     ada.talk
 
     def walk
-      "The Person is walking."
+      puts "The Person is walking."
       ada = Person.new
       ada.walk
     end
